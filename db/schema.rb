20ad9e69fc7d35dcb34fdf190dab051e038db4b9
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2026-02-10 22:43:15 +0700

    Remove recoverable and rememberable Devise modules and unused views

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_10_114449) do
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
